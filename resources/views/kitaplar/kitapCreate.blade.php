f5c6db8eba97c297694774baa140426129e13549
Nuri Akman <nuriakman@gmail.com>
2019-12-18 23:38:47 +0300

    Form guncellemesi

--- resources/views/kitaplar/kitapCreate.blade.php
+++ resources/views/kitaplar/kitapCreate.blade.php
@@ -4,10 +4,10 @@
 
 <form method='POST' action='{{ route("kitapStore") }}' autocomplete="off">
 	@csrf
-	<input type="text" name="kitapAdi"    placeholder="Kitap Adı"><br>
-	<input type="text" name="kitapYazari" placeholder="Kitap Yazarı"><br>
-	<input type="text" name="yayinYili"   placeholder="yayın Yılı"><br>
-	<input type="text" name="ozeti"       placeholder="Kitap Özeti"><br>
+	<input type="text" name="kitapAdi"    value="{{ old('kitapAdi') }}"    placeholder="Kitap Adı"><br>
+	<input type="text" name="kitapYazari" value="{{ old('kitapYazari') }}" placeholder="Kitap Yazarı"><br>
+	<input type="text" name="yayinYili"   value="{{ old('yayinYili') }}"   placeholder="yayın Yılı"><br>
+	<input type="text" name="ozeti"       value="{{ old('ozeti') }}"       placeholder="Kitap Özeti"><br>
 	<input type="submit" value="Kitap Ekle">
 </form>
 
